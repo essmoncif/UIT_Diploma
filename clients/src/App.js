b2e0ea6cffbb9e2a896715452c527253a4093884
essmoncif <moncefessaoudi@gmail.com>
2020-10-20 20:53:20 +0100

    create index route

--- clients/src/App.js
+++ clients/src/App.js
@@ -1,35 +1,18 @@
-import React, {Component} from 'react';
+import React from 'react';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import './App.css';
-import getWeb3 from './providers/web3.provider';
+import Index from './pages/index.page';
 
-class App extends Component {
-
-  state = {web3: null, accounts: null};
-
-
-  componentDidMount = async() => {
-    try {
-      const web3 = await getWeb3();
-      const accounts = await web3.eth.getAccounts();
-      this.setState({web3, accounts});
-    } catch (error) {
-      alert('Failed to load web3, accounts,')
-    }
-  }
-
-  render(){
-    if (!this.state.web3) {
-      return <div>Loading Web3, accounts, and contract...</div>;
-    }
-    return (
-      <div className="App">
-        <header className="App-header">
-         {this.state.accounts}
-        </header>
-      </div>
-    );
-  }
-  
+function App() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/">
+          <Index/>
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
