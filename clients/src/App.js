746b7e0c983c3b8cba779bd1d5463c72e952a49f
essmoncif <moncefessaoudi@gmail.com>
2020-10-20 13:14:00 +0100

    test MetaMask access

--- clients/src/App.js
+++ clients/src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import getWeb3 from './providers/web3.provider';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {web3: null, accounts: null};
+
+
+  componentDidMount = async() => {
+    try {
+      const web3 = await getWeb3();
+      const accounts = await web3.eth.getAccounts();
+      this.setState({web3, accounts});
+    } catch (error) {
+      alert('Failed to load web3, accounts,')
+    }
+  }
+
+  render(){
+    if (!this.state.web3) {
+      return <div>Loading Web3, accounts, and contract...</div>;
+    }
+    return (
+      <div className="App">
+        <header className="App-header">
+         {this.state.accounts}
+        </header>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
